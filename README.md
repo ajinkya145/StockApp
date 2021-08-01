b8d21fdd94045d8a27bce27edff3d169e963f8b5
Ajinkya Lahade <ajinkyal@cascadeloans.com>
2021-08-01 00:10:45 -0700

    First commit
    Let's try this

---
+++ README.md
@@ -0,0 +1,2 @@
+# This is a small web application that shows the evolution of TSLA stock price since January, 2005.
+## Enjoy!
---
+++ prophet.py
@@ -0,0 +1,116 @@
+#inspired by python engineer youtube 
+import streamlit as st
+from datetime import date
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+START = "2011-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title('STOCK PREDICTIONS WITH PROPHET')
+
+st.write('Select your stock')
+
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME','MSFT','TSLA','NVDA','SPY')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
+
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
+
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+	
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+# Plot raw data
+def plot_raw_data():
+	#get the charts for data visalization
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
+
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
+
+
+########################SPY ADDITION##################################
+
+
+@st.cache
+def load_data_spy(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+	
+data_load_state = st.text('Loading data for SPY...')
+data = load_data_spy('SPY')
+data_load_state.text('Loading data for SPY... done!')
+
+st.subheader('Raw data for SPY')
+st.write(data.tail())
+
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='SPY CORR', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
+
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data for SPY')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("FBPROPHET")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
